eab0c63d09d92aa347e5b986ddd4a18f3ce2f37c
Matthew Bare <mcbare@ad.unc.edu>
2022-04-08 14:03:50 -0400

    Footer component created with basic styling

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,9 @@
-export function footer() {
+import styles from '../styles/Footer.module.css'
+
+export function Footer() {
     return (
-        <div></div>
-    )
+        <div className={styles.main}>
+            <p>Created by: Andrew Byerle, Ashwin Yeramsetty, Matthew Bare, Sofia Wong, Winfield Warren</p>
+        </div>
+    );
 }
