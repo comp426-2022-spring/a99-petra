5d58dc091fe4c78193f8c62583be29aebe5a546c
Matthew Bare <mcbare@ad.unc.edu>
2022-04-11 16:11:05 -0400

    Final footer changes before initial pull request

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 export function Footer() {
     return (
         <div className={styles.main}>
-            <p>Created by: Andrew Byerle, Ashwin Yeramsetty, Matthew Bare, Sofia Wong, Winfield Warren</p>
+            <p>Created by: Andrew Byerle, Ashwin Yerramsetty, Matthew Bare, Sofia Wong, Winfield Warren</p>
         </div>
     );
 }
