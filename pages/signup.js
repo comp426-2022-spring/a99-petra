d3b265b6c9949b0c8821661f7a030253a5a2097a
Winfield Warren <winfieldwarren@unc.edu>
2022-04-18 16:40:20 -0400

    updated sign up

--- pages/signup.js
+++ pages/signup.js
@@ -2,59 +2,83 @@ import React, { useEffect, useState } from "react";
 
 function SignUp(Component) {
 
-    /*
-    const [data, setData] = useState({
-        firstname: "",
-        lastname: "",
-    });
+    const [data,setData] = useState({
+        firstname:"",
+        lastname:"",
+        email:"",
+        phone:"",
+        username:"",
+        password:"",
+        repeatpassword:""
 
-    function handleChange(event) {
-        setData({...data, [event.target.name]:[event.target.value]});
-    }
+      });
 
-    function handleSubmit(event) {
-        alert("firstname" + data.firstname);
-    }*/
-
-    return (
+      
+    
+      const handleSubmit = e => {
         
-        <div className='Inputs'>
+      }
+    
+      const changeUpdate = e => {
+        setData({...data,[e.target.name]:[e.target.value]});
+      }
+
+      /*const changePassword = e => {
+        setData({...data,[e.target.name]:[e.target.value]});
+        if(data.password != data.repeatpassword) {
+            document.getElementById("submit").disabled=true;
+        }
+        else{
+            document.getElementById("submit").disabled=false;
+        }
+      }*/
+
+      const {firstname, lastname, email, phone, username,password, repeatpassword} = data;
+
+      //var disabled = false;
+
+    
+
+    return ( 
+        
+        <div className="Inputs">
             <form>
-                <label for='first-name'>
+                <label for="firstname">
                     First Name: 
-                    <input type='text' placeholder='John' name='first-name' required></input>
+                    <input type="text" placeholder="John" name="firstname" value={firstname} onChange={changeUpdate} required></input>
                 </label>
                 <br></br>
-                <label for='last-name'>
+                <label for="lastname">
                     Last Name: 
-                    <input type='text' placeholder='Doe' name='last-name' required></input>
+                    <input type="text" placeholder="Doe" name="lastname" value={lastname} onChange={changeUpdate} required></input>
                 </label>
                 <br></br>
-                <label for='email'>
+                <label for="email">
                     Email: 
-                    <input type='text' placeholder='johndoe@email.com' name='email' required></input>
+                    <input type="text" placeholder="johndoe@email.com" name="email" value={email} onChange={changeUpdate} required></input>
                 </label>
                 <br></br>
-                <label for='phone'>
+                <label for="phone">
                     Phone Number: 
-                    <input type='text' placeholder='123-456-7890' name='phone' required></input>
+                    <input type="text" placeholder="123-456-7890" name="phone" value={phone} onChange={changeUpdate} required></input>
                 </label>
                 <br></br>
-                <label for='user-name'>
+                <label for="username">
                     User Name: 
-                    <input type='text' placeholder='johndoe' name='user-name' required></input>
+                    <input type="text" placeholder="johndoe" name="username" value={username} onChange={changeUpdate} required></input>
                 </label>
                 <br></br>
-                <label for='password'>
+                <label for="password">
                     Password: 
-                    <input type='password' placeholder='Enter Password' name='password' required></input>
+                    <input type="password" placeholder="Enter Password" name="password" value={password} onChange={changeUpdate} required></input>
                 </label>
                 <br></br>
-                <label for='repeat-password'>
+                <label for="repeatpassword">
                     Repeat Password: 
-                    <input type='password' placeholder='Enter Your Password Again' name='repeat-password' required></input>
+                    <input type="password" placeholder="Enter Your Password Again" name="repeatpassword" value={repeatpassword} onChange={changeUpdate} required></input>
+                    <span name="confirmation"></span>
                 </label>
-                <input type='submit' value='Submit'></input>
+                <input type="submit" id="submit" value="Submit" onSubmit={handleSubmit} disabled={false}></input>
             </form>
         </div>
         
