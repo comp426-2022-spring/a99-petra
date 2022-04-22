2560d2b7002126269adef6d7c9a1d7cfa585c371
Sofia Wong <ssofia@ad.unc.edu>
2022-04-22 09:50:18 -0400

    adding some covid info to dash

--- data/covidData.js
+++ data/covidData.js
@@ -1,20 +1,31 @@
 import axios from "axios";
 
 
-export const getCovidData = async () => {
+export const getCovidData = async (setCasesInfo, setDeathInfo, setPos, setNeg) => {
 
     const API_KEY = process.env.NEXT_PUBLIC_COVID_KEY;
     const state = "CA"
 
+    
+
     const currentUSData = `https://api.covidactnow.org/v2/country/US.json?apiKey=${API_KEY}`;
     // const historicUSData = `https://api.covidactnow.org/v2/country/US.timeseries.json?apiKey=${API_KEY}`
     // const dataByState = `https://api.covidactnow.org/v2/state/${state}.json?apiKey=${API_KEY}`
+    
 
     axios.get(currentUSData).then((res) => {
         console.log(res.data)
-        // const cases = res.data.actuals.cases;
+        const cases = res.data.actuals.cases;
+        setCasesInfo(cases)
         // const deaths = res.data.actuals.deaths;
+        const deaths = res.data.actuals.deaths;
+        setDeathInfo(deaths)
 
+        const pos = res.data.actuals.positiveTests
+        setPos(pos);
+
+        const neg = res.data.actuals.negativeTests
+        setNeg(neg);
 
         // console.log("cases: ", cases, " deaths: ", deaths)
         return { data: res.data };
@@ -23,6 +34,8 @@ export const getCovidData = async () => {
             console.log(e)
             return { success: false, message: e.message };
         });
+
+        
 
 
 };
--- pages/simpleDashboard.js
+++ pages/simpleDashboard.js
@@ -1,11 +1,44 @@
 import React, { useEffect, useState } from "react";
 import styles from '../styles/dash.module.css';
+import { PieChart, Pie, Tooltip, Cell,  Sector} from 'recharts';
+
+import {getCovidData} from '../data/covidData'
+
+//To do:
+//Variables for Profile
+//Dont display profile if signed in
+//Legend for graph
+//color changes
+
+
 
 export default function Home() {
+    const [casesInfo, setCasesInfo] = useState(0)
+    const [deathInfo, setDeathInfo] = useState(0)
+
+    var loggedIn = false;
+    const COLORS = ['#0088FE', '#00C49F'];
+
+    const [pos, setPos] = useState(0)
+    const [neg, setNeg] = useState(0)
+    
+    useEffect(() => { //fired once after things are rendered - as soon as page loads 
+        getCovidData(setCasesInfo, setDeathInfo, setPos, setNeg);
+    }, [casesInfo], [deathInfo], [pos], [neg]) //array -> use effect fires when this var changes
+    
+
+    const data = [
+        {name: 'Positive Results', students: pos},
+        {name: 'Negative Results', students: neg},
+      ];
+
+      console.log(getCovidData);
+
     return (
+        
         <div className={styles.grid}>
             <div className={styles.header}>
-                HEADER
+                COVID DASHBOARD
             </div>
             <div className={styles.sidebar}>
                 <a className={styles.profile} href="http://localhost:3000/simpleProfile">
@@ -27,20 +60,41 @@ export default function Home() {
             </div>
             
             <div className={styles.bigBox}>
-                THIS IS THE BIG BOX - MAYBE GRAPH OR PIE CHART HERE
-                <div className={styles.circle2}>
-                    
-                    </div>
+                NEGATIVE TESTS VS POSITIVE TEST
+                
+                <PieChart width={400} height={500}>
+                    <Pie data={data} dataKey="students" outerRadius={200} fill= 'aquamarine' />
+                    {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                    <Tooltip />
+                
+                </PieChart>
+        
             </div>
             <div className={styles.smallBox1}>
-                THIS IS SMALL BOX 1 FOR SOME STATS
+                Current cases:
+                <b><div className={styles.stats}>
+                {casesInfo} 
+                {/* if there are issues look up turnary opperator (like an if else)  */}
+                </div></b>
+
             </div>
             <div className={styles.smallBox2}>
-                THIS IS SMALL BOX 2 FOR SOME STATS
+                Current Deaths:
+                <b> <div className={styles.stats}> 
+                {deathInfo} 
+                {/* if there are issues look up turnary opperator (like an if else)  */}
+                </div> </b>
             </div> 
 
             <div className={styles.footer}>
-                THIS IS A PLACEHOLDER FOR THE FOOTER
+                Links for more recources:
+                <div className={styles.footLinks}>
+                    <a href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">CDC Website</a>
+                    <a href="https://www.covidtests.gov/">Order Free Covid Tests</a>
+                    <a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/faq.html">Questions on Vaccines</a>
+                </div>
             </div>
         </div>
     )
