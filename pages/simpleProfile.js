6647c97dc50061cf52a303d2b1259a9857909e68
Andrew Byerle <andran@live.unc.edu>
2022-04-27 12:08:15 -0400

    added exit button to user profile page

--- pages/simpleProfile.js
+++ pages/simpleProfile.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import styles from '../styles/profile.module.css';
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import Router from "next/router";
+import { ImCross } from 'react-icons/im';
+import Link from "next/link";
+
 
 
 export default function Home() {
@@ -42,21 +45,27 @@ export default function Home() {
 
     return (
         <div className={styles.contain}>
+
             <div className={styles.profile}>
+                <Link href="/simpleDashboard">
+                    <div className={styles.exitButton}>
+                        <ImCross></ImCross>
+                    </div>
+                </Link>
                 <div className={styles.circle}>
 
                 </div>
                 <div>
                     {userData != null ? userData.firstname : ""}
-                     <br></br>
-                    {userData != null ? userData.lastname: ""}
+                    <br></br>
+                    {userData != null ? userData.lastname : ""}
                 </div>
 
                 <div>
 
                     Last Login Date: {userData != null ? userData.lastLoginDate : ""} <br></br>
-                    Email: {userData != null ? userData.email: ""}<br></br>
-                    Phone: {userData != null ? userData.phone: ""} <br></br>
+                    Email: {userData != null ? userData.email : ""}<br></br>
+                    Phone: {userData != null ? userData.phone : ""} <br></br>
                 </div>
 
                 <a className={styles.button} href='/editUserInfo'>
